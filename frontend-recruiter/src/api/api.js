f36e3dda777f49875b361df30d36afae57a1c820
bhupendrajs98 <bhupendrajsx98@gmail.com>
2026-01-09 01:32:29 +0530

    env fix

---
+++ frontend-recruiter/src/api/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("recruiter_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default API;
